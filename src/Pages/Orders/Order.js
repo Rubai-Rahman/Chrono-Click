559ab91caf21293711991e96a17b0a4a711cedca
RubaiRahman <rubairahman1@gmail.com>
2022-10-17 17:54:45 +0600

    car completed

--- src/Pages/Orders/Order.js
+++ src/Pages/Orders/Order.js
@@ -1,11 +1,57 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Button, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import useAuth from "../../hooks/useAuth";
+import {AiFillDelete} from "react-icons/ai"
 
 const Order = () => {
+  const { state: { cart }, dispatch } = useAuth();
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc,curr) => acc+Number(curr.price),0))
+  },[cart])
   return (
-    <div>
-      <h1>this is order</h1>
-    </div>
-  );
+    <Container>
+      <div>
+        <ListGroup>
+          {cart.map((product) => (
+            <ListGroupItem>
+              <Row>
+                <Col>
+                  <img
+                    src={product.img}
+                    style={{ width: 50, height: 50 }}
+                    alt="pImage"
+                  />
+                </Col>
+                <Col>{product.name}</Col>
+                <Col>{product.price}</Col>
+                <Col>
+                  {" "}
+                  <AiFillDelete
+                    style={{ cursor: "pointer", fontSize: 20 }}
+                    onClick={() =>
+                      dispatch({
+                        type: "REMOVE_FROM_CART",
+                        payload: product,
+                      })
+                    }
+                  />
+                </Col>
+              </Row>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
+      <div className="filters summary ">
+        <span className="title">Subtotal ({cart.length}) items</span>
+        <span className="title"> Total: $ {total} </span>
+        <button className="bbutton">Check Out</button>
+      </div>
+    </Container>
+  )
 };
 
 export default Order;
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Container, Navbar, Nav, Button } from "react-bootstrap";
-import {  NavLink } from "react-router-dom";
+import { Container, Navbar, Nav, Button, Dropdown, Badge } from "react-bootstrap";
+import {  Link, NavLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 import logo from "../../../images/favicon.png";
 import "./Navigation.css";
+import { FaShoppingCart } from "react-icons/fa"
+import { AiFillDelete } from "react-icons/ai"
 
 const Navigation = () => {
   const {
-    allContexts: { user, logOut },
+    allContexts: { user, logOut },state:{cart},dispatch
   } = useAuth()
   let activeColor = {
     color: "#9c7c38",
@@ -15,6 +17,13 @@ const Navigation = () => {
   let deActiveColor = {
     color: "#838996",
   };
+   const CartTitleStyle = {
+     width: 20,
+     overflow: "hidden",
+     textOverflow: "hidden",
+     whiteSpace: "nowrap",
+     margin: 10,
+   }
   return (
     <Navbar bg="dark" expand="lg">
       <Container fluid>
@@ -54,27 +63,82 @@ const Navigation = () => {
               Order
             </NavLink>
           </Nav>
+          <Nav>
+            <Dropdown>
+              <Dropdown.Toggle variant="dark">
+                <FaShoppingCart color="white" fontSize="25px" />
+                <Badge variant="dark">{cart.length}</Badge>
+              </Dropdown.Toggle>
+              <Dropdown.Menu
+                align="end"
+                style={{
+                  minWidth: 370,
+                  height: 500,
+                  overflow: "scroll",
+                  paddingLeft: 30,
+                  marginTop: 20,
+                }}
+              >
+                {cart.length > 0 ? (
+                  <>
+                    {cart.map((product) => (
+                      <span className="cartItem">
+                        <img className="cartItemImg" src={product.img} alt="" />
+                        <div className="cartItemDetails">
+                          <span style={CartTitleStyle}>{product.title}</span>
+                          <span style={{ margin: 10, color: "green" }}>
+                            ${product.price}
+                          </span>
+                        </div>
+                        <AiFillDelete
+                          style={{ cursor: "pointer", fontSize: 20 }}
+                          onClick={() =>
+                            dispatch({
+                              type: "REMOVE_FROM_CART",
+                              payload: product,
+                            })
+                          }
+                        />
+                      </span>
+                    ))}
+                    <Link to="/order">
+                      <button
+                        className="bbutton"
+                      >
+                        Order
+                      </button>
+                    </Link>
+                  </>
+                ) : (
+                  <span style={{ padding: 10 }}> Cart is Empty</span>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
           {user?.email ? (
             <Button
               style={{ backgroundColor: "transparent", border: "none" }}
               onClick={logOut}
               className="nav_link"
             >
-              <ion-icon className='profileButton' name="person-circle-outline"></ion-icon>
+              <ion-icon
+                className="profileButton"
+                name="person-circle-outline"
+              ></ion-icon>
             </Button>
           ) : (
-            
             <NavLink
-            to="/login" className="nav_link link2"
-            style={({ isActive }) => (isActive ? activeColor : deActiveColor)}
-          >
-            LogIn
-          </NavLink>
+              to="/login"
+              className="nav_link link2"
+              style={({ isActive }) => (isActive ? activeColor : deActiveColor)}
+            >
+              LogIn
+            </NavLink>
           )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+  )
 };
 
 export default Navigation;
