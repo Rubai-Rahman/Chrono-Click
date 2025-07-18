23330468178dd04693ca66acac8a3b69d3194cbd
Rubai Rahman <rubairahman1@gmail.com>
2025-07-18 15:51:48 +0600

    navbar updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import Slider from "@/components/main/home/Slider";
-import Products from "@/components/main/home/Products";
-import Banner from "@/components/main/home/Banner";
-import Testimonials from "@/components/main/home/Testimonials";
-import News from "@/components/main/home/News";
-import Newsletter from "@/components/main/home/Newsletter";
-import type { Metadata } from "next";
+import React from 'react';
+import Slider from '@/components/main/home/Slider';
+import Products from '@/components/main/home/Products';
+import Banner from '@/components/main/home/Banner';
+import Testimonials from '@/components/main/home/Testimonials';
+import News from '@/components/main/home/News';
+import Newsletter from '@/components/main/home/Newsletter';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Chrono Click - Home",
-  description: "Your ultimate destination for stylish watches and accessories.",
+  title: 'Chrono Click - Home',
+  description: 'Your ultimate destination for stylish watches and accessories.',
 };
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="">
       <Slider />
       <Products />
       <Banner />
