5511d557709b18844f7f8034ea80189c172efd5b
Rubai Rahman <rubairahman1@gmail.com>
2025-07-20 17:08:42 +0600

    banner added

--- src/components/main/home/Banner.tsx
+++ src/components/main/home/Banner.tsx
@@ -1,22 +1,21 @@
-'use client'
-import React, { useState } from "react";
-import { PlayCircle, X } from "lucide-react";
+'use client';
+import React, { useState } from 'react';
+import { PlayCircle, X } from 'lucide-react';
 
 const Banner = () => {
   const [show, setShow] = useState(false);
-  const openModal = () => {
-    setShow(true);
-  };
-  const closeModal = () => {
-    setShow(false);
-  };
 
   return (
-    <div className="relative bg-gray-800 text-white py-16 px-4 text-center">
+    <div className="relative bg-[url('/banner.webp')] bg-no-repeat bg-cover bg-blend-darken bg-[#b7b7d5] flex flex-col items-center justify-center text-primary-foreground text-center  h-[400px] sm:h-[300px] md:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px] px-4">
       <div className="container mx-auto">
-        <h3 className="text-3xl font-bold mb-2">Chrono Click ... Time Is Ticking Away!</h3>
-        <h5 className="text-xl mb-6">DECIDE NOW TO UPDATE</h5>
-        <button className="bg-transparent border-none text-white text-5xl cursor-pointer" onClick={openModal}>
+        <h3 className="text-3xl font-bold mb-2">
+          Chrono Click ... Time Is Ticking Away!
+        </h3>
+        <h5 className=" mb-6">DECIDE NOW TO UPDATE</h5>
+        <button
+          className="bg-transparent border-none text-white text-5xl cursor-pointer"
+          onClick={() => setShow(true)}
+        >
           <PlayCircle size={64} />
         </button>
       </div>
@@ -26,17 +25,18 @@ const Banner = () => {
           <div className="bg-black p-4 rounded-lg relative">
             <button
               className="absolute top-2 right-2 bg-transparent border-none text-yellow-600 text-5xl cursor-pointer"
-              onClick={closeModal}
+              onClick={() => setShow(false)}
             >
               <X size={32} />
             </button>
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/embed/p0ZHRJ0xcd8"
+              src="https://www.youtube.com/embed/efqKWzXIqEI?si=0GZQJs04eopvjOUI"
               title="YouTube video player"
               frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
             ></iframe>
           </div>
