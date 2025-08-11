f513515adfd211e0d6956cab9a13a211d06b1379
Rubai Rahman <rubairahman1@gmail.com>
2025-08-11 17:03:33 +0600

    small issue fix regarding layout

--- src/components/admin/admin-layout.tsx
+++ src/components/admin/admin-layout.tsx
@@ -16,18 +16,26 @@ interface AdminLayoutProps {
 
 export function AdminLayout({ children }: AdminLayoutProps) {
   return (
-    <SidebarProvider>
-      <AdminSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
-      </SidebarInset>
+    <SidebarProvider className="responsive-space-x  container mx-auto">
+      <div className="grid grid-cols-[auto_1fr]">
+        <div className="h-screen sticky top-0">
+          <AdminSidebar />
+        </div>
+        <div>
+          <SidebarInset>
+            <header className="sticky top-0 z-10 bg-sidebar flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4 ">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb />
+              </div>
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+              {children}
+            </div>
+          </SidebarInset>
+        </div>
+      </div>
     </SidebarProvider>
   );
 }
