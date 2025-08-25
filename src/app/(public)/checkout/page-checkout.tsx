86ad3278af24977fdcbc11d67fc22ac6707586b6
Rubai Rahman <rubairahman1@gmail.com>
2025-08-25 06:56:23 +0600

    brands updated

--- src/app/(public)/checkout/page-checkout.tsx
+++ src/app/(public)/checkout/page-checkout.tsx
@@ -1,4 +1,3 @@
-'use client';
 
 import OrderSummary from '@/components/checkout/order-summary';
 import CheckoutForm from '@/components/checkout/checkout-form';
--- src/components/main/home/brands.tsx
+++ src/components/main/home/brands.tsx
@@ -1,5 +1,5 @@
+import BrandItem from '@/components/brands/branditem';
 import Container from '@/components/layout/container';
-
 
 const Brands = () => {
   return (
@@ -7,12 +7,9 @@ const Brands = () => {
       <div className="bg-gradient-to-br from-primary/5 to-accent/5 ">
         <div className="bg-card rounded-2xl p-6 ">
           <div className="text-center mb-8 sm:mb-12">
-            <h2 className="text-2xl font-semibold text-primary mb-4">
-              OUR BRANDS
-            </h2>
+            <h2 className="text-2xl font-semibold text-primary">OUR BRANDS</h2>
           </div>
-
-         
+          <BrandItem />
         </div>
       </div>
     </Container>
