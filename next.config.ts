7e2d0c6b657b6191509a6d3b59707e5990ecbe91
Rubai Rahman <rubairahman1@gmail.com>
2025-08-25 19:29:33 +0600

    Clear Git cache for case sensitivity

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: '.next',
+  cleanDistDir: true,
+  poweredByHeader: false,
   images: {
     remotePatterns: [
       {
